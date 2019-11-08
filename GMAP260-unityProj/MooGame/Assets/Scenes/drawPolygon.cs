14c7f37d9af922afefbaac875fa3655849aad7db
mmelkumyan <47286353+mmelkumyan@users.noreply.github.com>
2019-11-08 16:13:32 -0500

    tested line renderer

---
+++ GMAP260-unityProj/MooGame/Assets/Scenes/drawPolygon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawPolygon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //counter checks for every 60 frames
+        //after 60 frames call makePoint function
+        //makePoint adds position to array
+        //make lines between all pairs of points
+        //check if lines cross OR if two points are vey close
+        //pass points to checkForCows function
+        //after x seconds, remove top point from queue
+    }
+}
